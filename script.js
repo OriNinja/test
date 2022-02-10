8b977747ba699bf0ea932ee390d29e68540478cc
OriNinja <95003779+OriNinja@users.noreply.github.com>
2022-02-10 10:04:48 -0800

    message

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function hideMenu() {
   notMenu.style.display = "block";
   onMenu.style.display = "none";
 };
+
+var mobile = document.getElementById("mobile-view")
+var desktop = document.getElementById("desktop-view")
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  desktop.style = "display: none"
+}else{
+  mobile.style = "display: none"
+}
